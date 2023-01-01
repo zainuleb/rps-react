0494fe07366a5444c47a9b2e56ebfb0d3b40515c
zainuleb <zainulebadd@gmail.com>
2023-01-02 03:59:42 +0500

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Menu from './components/menu/Menu';
+import PNF from './components/pnf/PNF';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Menu />
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="*" element={<PNF />} />
+      </Routes>
+
       <Footer />
     </div>
   );
---
+++ src/components/pnf/PNF.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const PNF = () => {
+  return (
+    <section class="fore-zero padding-top padding-bottom">
+      <div class="container">
+        <div class="section-wrapper">
+          <div class="zero-item">
+            <div class="zero-thumb">
+              <img src="assets/images/404.png" alt="404" />
+            </div>
+            <div class="zero-content">
+              <h2>Oops! This Page Not Found</h2>
+              <p>
+                We are Really Sorry But the Page you Requested is Missing
+                <i class="icofont-worried"></i>
+              </p>
+              <a href="index.html" class="default-button reverse-effect">
+                <span>
+                  Go Back to Home <i class="icofont-double-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PNF;

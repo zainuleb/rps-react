d8452c0219cb269a69ddbe9e601ccbbcc497e0d4
zainuleb <zainulebadd@gmail.com>
2023-01-04 02:19:15 +0500

    single player completed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,50 +2,50 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="footer-section">
-      <div class="footer-top">
-        <div class="container">
-          <div class="row g-3 justify-content-center g-lg-0">
-            <div class="col-lg-4 col-sm-6 col-12">
-              <div class="footer-top-item lab-item">
-                <div class="lab-inner">
-                  <div class="lab-thumb">
+    <footer className="footer-section">
+      <div className="footer-top">
+        <div className="container">
+          <div className="row g-3 justify-content-center g-lg-0">
+            <div className="col-lg-4 col-sm-6 col-12">
+              <div className="footer-top-item lab-item">
+                <div className="lab-inner">
+                  <div className="lab-thumb">
                     <img
                       src="assets/images/footer/icons/01.png"
                       alt="Phone-icon"
                     />
                   </div>
-                  <div class="lab-content">
+                  <div className="lab-content">
                     <span>Phone Number : +9290078601</span>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-6 col-12">
-              <div class="footer-top-item lab-item">
-                <div class="lab-inner">
-                  <div class="lab-thumb">
+            <div className="col-lg-4 col-sm-6 col-12">
+              <div className="footer-top-item lab-item">
+                <div className="lab-inner">
+                  <div className="lab-thumb">
                     <img
                       src="assets/images/footer/icons/02.png"
                       alt="email-icon"
                     />
                   </div>
-                  <div class="lab-content">
+                  <div className="lab-content">
                     <span>Email : rockpaperscissor@gmail.com</span>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-6 col-12">
-              <div class="footer-top-item lab-item">
-                <div class="lab-inner">
-                  <div class="lab-thumb">
+            <div className="col-lg-4 col-sm-6 col-12">
+              <div className="footer-top-item lab-item">
+                <div className="lab-inner">
+                  <div className="lab-thumb">
                     <img
                       src="assets/images/footer/icons/03.png"
                       alt="location-icon"
                     />
                   </div>
-                  <div class="lab-content">
+                  <div className="lab-content">
                     <span>Address : Frankfurt Oder</span>
                   </div>
                 </div>
@@ -55,64 +55,14 @@ const Footer = () => {
         </div>
       </div>
       <div
-        class="footer-middle padding-top padding-bottom"
+        className="footer-middle padding-top padding-bottom"
         style={{ backgroundImage: `url(assets/images/footer/bg.jpg)` }}
-      >
-        <div class="container">
-          <div class="row padding-lg-top">
-            <div class="col-lg-4 col-md-6 col-12">
-              <div class="footer-middle-item-wrapper">
-                <div class="footer-middle-item mb-lg-0">
-                  <div class="fm-item-title mb-4">
-                    <img src="assets/images/logo/logo.png" alt="logo" />
-                  </div>
-                  <div class="fm-item-content">
-                    <p class="mb-4">--- Content to Write ---</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-12"></div>
-            <div class="col-lg-4 col-md-6 col-12">
-              <div class="footer-middle-item-wrapper">
-                <div class="footer-middle-item-3 mb-lg-0">
-                  <div class="fm-item-title">
-                    <h4>Our Newsletter</h4>
-                  </div>
-                  <div class="fm-item-content">
-                    <form>
-                      <div class="form-group mb-4">
-                        <input
-                          type="text"
-                          class="form-control"
-                          placeholder="Your Name"
-                        />
-                      </div>
-                      <div class="form-group mb-2">
-                        <input
-                          type="email"
-                          class="form-control"
-                          placeholder="Your Email"
-                        />
-                      </div>
-                      <button type="submit" class="default-button">
-                        <span>
-                          Send Massage <i class="icofont-circled-right"></i>
-                        </span>
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="footer-bottom">
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <div class="footer-bottom-content text-center">
+      ></div>
+      <div className="footer-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="footer-bottom-content text-center">
                 <p>
                   &copy;2023 <a href="/#">RPS</a>
                 </p>
--- src/components/single-game/SingleGame.js
+++ src/components/single-game/SingleGame.js
@@ -1,56 +1,121 @@
 import React, { useState } from 'react';
 
 const SingleGame = () => {
-  const [userSelection, setUserSelection] = useState(0);
+  const [userSelection, setUserSelection] = useState('➖');
+  const [compSelection, setCompSelection] = useState('➖');
+
+  const [userScore, setUserScore] = useState(0);
+  const [compScore, setCompScore] = useState(0);
+
+  const [userWin, setUserWin] = useState();
+
   const userSelectionFunction = (param) => {
     setUserSelection(param);
+    let randVal = Math.floor(Math.random() * 3);
+    switch (randVal) {
+      case 0:
+        setCompSelection('🪨');
+        if (param === '📄') userWinFunc();
+        if (param === '✂️') compWinFunc();
+        if (param === '🪨') drawFunc();
+        break;
+      case 1:
+        setCompSelection('📄');
+        if (param === '✂️') userWinFunc();
+        if (param === '🪨') compWinFunc();
+        if (param === '📄') drawFunc();
+        break;
+      case 2:
+        setCompSelection('✂️');
+        if (param === '🪨') userWinFunc();
+        if (param === '📄') compWinFunc();
+        if (param === '✂️') drawFunc();
+        break;
+
+      default:
+        break;
+    }
   };
+
+  const userWinFunc = () => {
+    setUserScore(() => userScore + 1);
+    setUserWin(() => true);
+  };
+
+  const compWinFunc = () => {
+    setCompScore(() => compScore + 1);
+    setUserWin(() => false);
+  };
+
+  const drawFunc = () => {
+    console.log('Draw');
+  };
+
   return (
-    <div className="sg-container padding-top">
-      <div className="computer-side">
-        <h1>
-          <span>L</span>
-          <span>E</span>
-          <span>T</span>
-          <span>'</span>
-          <span>S</span>
-          <span id="whitespace"></span>
-          <span>P</span>
-          <span>L</span>
-          <span>A</span>
-          <span>Y</span>
-          <span>!</span>
-        </h1>
+    <div className="padding-top">
+      <div
+        className="score-container"
+        style={{ backgroundColor: userWin === true ? 'green' : 'red' }}
+      >
+        <h2>Computer</h2>
+        <h2>You</h2>
       </div>
-      <div className="v-line"></div>
-      <div className="user-side">
-        <div className="symbols-container">
-          <div onClick={() => userSelectionFunction('🪨')}>
-            <img
-              className="gameButton rock"
-              src="https://github.com/praxeds/theodinproject-rock-paper-scissors/blob/main/assets/images/raised-fist_270a.png?raw=true"
-              alt="Raised fist"
-            />
+      <div className="sg-container">
+        <div className="computer-side">
+          <div className="text-container">
+            <h1>
+              <span>L</span>
+              <span>E</span>
+              <span>T</span>
+              <span>'</span>
+              <span>S</span>
+              <span id="whitespace"></span>
+              <span>P</span>
+              <span>L</span>
+              <span>A</span>
+              <span>Y</span>
+              <span>!</span>
+            </h1>
           </div>
-          <div>
-            <img
-              onClick={() => userSelectionFunction('📄')}
-              className="gameButton paper"
-              src="https://github.com/praxeds/theodinproject-rock-paper-scissors/blob/main/assets/images/hand-with-fingers-splayed_1f590-fe0f.png?raw=true"
-              alt="Hand with fingers splayed"
-            />
+          <div className="left-emoji">
+            <h2>{compSelection}</h2>
           </div>
-          <div>
-            <img
-              onClick={() => userSelectionFunction('✂️')}
-              className="gameButton scissors"
-              src="https://github.com/praxeds/theodinproject-rock-paper-scissors/blob/main/assets/images/victory-hand_270c-fe0f.png?raw=true"
-              alt="Victory hand"
-            />
+          <div className="score-container-left">
+            <h2>{compScore}</h2>
           </div>
         </div>
-        <div>
-          <h2>{userSelection}</h2>
+        <div className="user-side">
+          <div className="symbols-container">
+            <div onClick={() => userSelectionFunction('🪨')}>
+              <img
+                className="gameButton rock"
+                src="https://github.com/praxeds/theodinproject-rock-paper-scissors/blob/main/assets/images/raised-fist_270a.png?raw=true"
+                alt="Raised fist"
+              />
+            </div>
+            <div>
+              <img
+                onClick={() => userSelectionFunction('📄')}
+                className="gameButton paper"
+                src="https://github.com/praxeds/theodinproject-rock-paper-scissors/blob/main/assets/images/hand-with-fingers-splayed_1f590-fe0f.png?raw=true"
+                alt="Hand with fingers splayed"
+              />
+            </div>
+            <div>
+              <img
+                onClick={() => userSelectionFunction('✂️')}
+                className="gameButton scissors"
+                src="https://github.com/praxeds/theodinproject-rock-paper-scissors/blob/main/assets/images/victory-hand_270c-fe0f.png?raw=true"
+                alt="Victory hand"
+              />
+            </div>
+          </div>
+          <div className="right-emoji">
+            <h2>{userSelection}</h2>
+          </div>
+          <div className="score-container-right">
+            <h2>{userScore}</h2>
+          </div>
         </div>
       </div>
     </div>

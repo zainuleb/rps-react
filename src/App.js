f0d4a7b97fa18d4c198be1f5dfecfde7f6b84c93
zainuleb <zainulebadd@gmail.com>
2023-01-17 04:55:55 +0500

    code comment

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-//Importing components
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Menu from './components/menu/Menu';
-import PNF from './components/pnf/PNF';
-import SingleGame from './components/single-game/SingleGame';
+// Importing components
+import Footer from './components/footer/Footer'; // Footer component
+import Header from './components/header/Header'; // Header component
+import Menu from './components/menu/Menu'; // Menu component
+import PNF from './components/pnf/PNF'; // 404 page component
+import SingleGame from './components/single-game/SingleGame'; // Single game component
 
-import { Routes, Route } from 'react-router-dom';
-import MultiPlayer from './components/multi-player/MultiPlayer';
+import { Routes, Route } from 'react-router-dom'; // Importing routing components from react-router-dom library
+import MultiPlayer from './components/multi-player/MultiPlayer'; // Multi player component
 
+// App component
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header /> {/* Renders the Header component */}
       <Routes>
+        {/*Default route, renders the Menu component */}
         <Route path="/" element={<Menu />} />
-        <Route
-          path="single-game"
-          element={<SingleGame title="Welcome" content="This is a modal" />}
-        />
+        {/* Single game route, renders the SingleGame component with props title and content */}
+        <Route path="single-game" element={<SingleGame />} />
+        {/* Multi game route, renders the MultiPlayer component */}
         <Route path="multi-game" element={<MultiPlayer />} />
-        <Route path="*" element={<PNF />} />
+        {/* 404 route, renders the PNF component */}
+        <Route path="" element={<PNF />} />
       </Routes>
-
-      <Footer />
+      <Footer /> {/* Renders the Footer component */}
     </div>
   );
 }
 
-export default App;
+export default App; // Exports the App component
